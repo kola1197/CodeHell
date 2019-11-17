ae6df7f100efd6f1d7e3b4f69a67b9d089d00eed
KrylovBoris <hakkanx@gmail.com>
2019-11-17 20:23:59 +0300

    main menu added

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Serialization;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject dialogueWindow;
+    public void StartNewGame()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    public void OpenDialogueWindow()
+    {
+        dialogueWindow.SetActive(true);
+    }
+    
+    public void CloseDialogueWindow()
+    {
+        dialogueWindow.SetActive(false);
+    }
+}
