988dc38692b39bcbb313c4f7c26d7b8243a90108
kola1197 <kola-1197@mail.ru>
2019-11-17 14:14:27 +0300

    Item1

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : Card
+{
+
+    public int power;
+    enum Type { CodeTroubles, SocialTroubles, OutsideTroubles }
+    Type type;
+    Item(string _about, string _title, int _power, Type _type) : base(_about, _title)
+    {
+        power = _power;
+        type = _type;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Person.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Person : MonoBehaviour
+{
+    public int power = 1;
+    List<Item> items = new List<Item>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    bool AddItem(Item item)
+    {
+        bool result = false;
+        if (items.Count < 5)
+        {
+            items.Add(item);
+            UpdatePower();
+            result = true;
+        }
+        return result;
+    }
+
+
+    bool RemoveItem(int i)
+    {
+        bool result = false;
+        if (items.Count > i)
+        {
+            items.RemoveAt(i);
+            UpdatePower();
+            result = true;
+        }
+        return result;
+    }
+
+    void UpdatePower()
+    {
+        foreach(Item item in items)
+        {
+            power += item.power;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
