babf603fbbc3a4cce0b590c367a69e93ae19452a
Gvalhca <Kir30041998>
2019-11-17 15:13:03 +0300

    Added Monster Cards Generation

--- Assets/Scripts/GameCards.cs
+++ Assets/Scripts/GameCards.cs
@@ -14,44 +14,59 @@ public class GameCards : MonoBehaviour
     public void GenerateCards()
     {
         mobsCards.Add(new Monster("Общение с клиентом", "Вы же сможете, вы же Эксперт!", 0, Monster.Type.CodeTroubles));
-        mobsCards.Add(new Monster("Отчёт", "Долбанные бумаги! Что? Это только за неделю?", 0, Monster.Type.CodeTroubles));
+        mobsCards.Add(
+            new Monster("Отчёт", "Долбанные бумаги! Что? Это только за неделю?", 0, Monster.Type.CodeTroubles));
         mobsCards.Add(new Monster("Дедлайн", "Вы и так его знаете...", 0, Monster.Type.CodeTroubles));
         mobsCards.Add(new Monster("Баг", "Несметные орды", 0, Monster.Type.CodeTroubles));
         mobsCards.Add(new Monster("Критическая ошибка",
             "Среди 400 предупреждений так легко пропустить действительно важные вещи", 0, Monster.Type.CodeTroubles));
         mobsCards.Add(new Monster("Забыли проверку на null", "Ошибка новичка", 0, Monster.Type.CodeTroubles));
         mobsCards.Add(new Monster("Провалы в текстуры", "Ловушка Джокера нервно курит", 0, Monster.Type.CodeTroubles));
-        mobsCards.Add(new Monster("Перепутали направление гравитации", "Или это был симулятор полета"));
-        mobsCards.Add(new Monster("Переобучение ии", "“Правильно” не всегда значит “хорошо”", 0, Monster.Type.CodeTroubles));
+        mobsCards.Add(new Monster("Перепутали направление гравитации", "Или это был симулятор полета", 0, Monster.Type.CodeTroubles));
+        mobsCards.Add(new Monster("Переобучение ии", "“Правильно” не всегда значит “хорошо”", 0,
+            Monster.Type.CodeTroubles));
         mobsCards.Add(new Monster("А что если так попробовать ?",
-            "Похоже на начало истории об отличном стартапе. Или на начало очень плохой идеи", 0, Monster.Type.CodeTroubles));
+            "Похоже на начало истории об отличном стартапе. Или на начало очень плохой идеи", 0,
+            Monster.Type.CodeTroubles));
         mobsCards.Add(new Monster("while (true))", "Бесконечность не предел!", 0, Monster.Type.CodeTroubles));
         mobsCards.Add(new Monster("Я художник, я так вижу!", "-Вася! Зачем 3d Берсерк?", 0, Monster.Type.CodeTroubles));
         mobsCards.Add(new Monster("Динамическая типизация", "Тут все и так ясно", 0, Monster.Type.CodeTroubles));
-        mobsCards.Add(new Monster("Переизбыток ctrl+c кода", "Жаль, что с деньгами так не выйдет...", 0, Monster.Type.CodeTroubles));
-        mobsCards.Add(new Monster("Неоднозначное тз", "Хочу то, сам не знаю чего, но чтобы принесли завтра!", 0, Monster.Type.CodeTroubles));
+        mobsCards.Add(new Monster("Переизбыток ctrl+c кода", "Жаль, что с деньгами так не выйдет...", 0,
+            Monster.Type.CodeTroubles));
+        mobsCards.Add(new Monster("Неоднозначное тз", "Хочу то, сам не знаю чего, но чтобы принесли завтра!", 0,
+            Monster.Type.CodeTroubles));
         mobsCards.Add(new Monster("Я не буду делать как удобно!", "Цитаты великих...", 0, Monster.Type.CodeTroubles));
         mobsCards.Add(new Monster("Похмелье", "Тише, пожалуйста", 0, Monster.Type.SocialTroubles));
         mobsCards.Add(new Monster("Новый сериал на нетфликс", "Надо попробовать в следующий раз рассказать им спойлеры",
             0, Monster.Type.SocialTroubles));
-        mobsCards.Add(new Monster("Токсичный тиммейт", "Действительно злой и неприятный", 0, Monster.Type.SocialTroubles));
-        mobsCards.Add(new Monster("Тиммейт - тормоз", "Делает быстро всего одну вещь — уходит.", 0, Monster.Type.SocialTroubles));
+        mobsCards.Add(new Monster("Токсичный тиммейт", "Действительно злой и неприятный", 0,
+            Monster.Type.SocialTroubles));
+        mobsCards.Add(new Monster("Тиммейт - тормоз", "Делает быстро всего одну вещь — уходит.", 0,
+            Monster.Type.SocialTroubles));
         mobsCards.Add(new Monster("Отсутствие консенсуса", "Лебедь, рак и щука", 0, Monster.Type.SocialTroubles));
         mobsCards.Add(new Monster("Отпуск у тимлида", "Кто снял замок с цепи?", 0, Monster.Type.SocialTroubles));
-        mobsCards.Add(new Monster("Расистские шуточки", "Негр и мексиканец падают с небоскрёба.Кто упадёт", 0, Monster.Type.SocialTroubles));
-        mobsCards.Add(new Monster("Всеобщая прострация", "С этим надо бы что - то сделать, но как - то все равно...", 0, Monster.Type.SocialTroubles));
-        mobsCards.Add(new Monster("Не сходящаяся бухгалтерия", "Не математики считают, что математики считают", 0, Monster.Type.SocialTroubles));
+        mobsCards.Add(new Monster("Расистские шуточки", "Негр и мексиканец падают с небоскрёба.Кто упадёт", 0,
+            Monster.Type.SocialTroubles));
+        mobsCards.Add(new Monster("Всеобщая прострация", "С этим надо бы что - то сделать, но как - то все равно...", 0,
+            Monster.Type.SocialTroubles));
+        mobsCards.Add(new Monster("Не сходящаяся бухгалтерия", "Не математики считают, что математики считают", 0,
+            Monster.Type.SocialTroubles));
         mobsCards.Add(new Monster("Закончились кофе и вода в кулере",
             "Когда я трезвый и пустой, я поворачиваюсь к миру темной стороной", 0, Monster.Type.SocialTroubles));
         mobsCards.Add(new Monster("Сломанный кондей", "Добро пожаловать в Сахару", 0, Monster.Type.SocialTroubles));
         mobsCards.Add(new Monster("Эпидемия гриппа", "Передайте...Ачпхи… чаю...", 0, Monster.Type.SocialTroubles));
-        mobsCards.Add(new Monster("Хейтеры", "Как думаете, они ненавидят всех или только вас?", 0, Monster.Type.OutsideTroubles));
-        mobsCards.Add(new Monster("Противники насилия", "Это они еще Доку 2 не видели...", 0, Monster.Type.OutsideTroubles));
+        mobsCards.Add(new Monster("Хейтеры", "Как думаете, они ненавидят всех или только вас?", 0,
+            Monster.Type.OutsideTroubles));
+        mobsCards.Add(new Monster("Противники насилия", "Это они еще Доку 2 не видели...", 0,
+            Monster.Type.OutsideTroubles));
         mobsCards.Add(new Monster("Проблемы с авторскими правами",
-            "Экономить на композиторе и просто вставлять треки Audiomachine было не лучшей идеей", 0, Monster.Type.OutsideTroubles));
+            "Экономить на композиторе и просто вставлять треки Audiomachine было не лучшей идеей", 0,
+            Monster.Type.OutsideTroubles));
         mobsCards.Add(new Monster("Феминистки", "Вообще не понятно, откуда они тут", 0, Monster.Type.OutsideTroubles));
-        mobsCards.Add(new Monster("Проблемы с балансом", "Даже в тетрисе были квадратные блоки", 0, Monster.Type.OutsideTroubles));
-        mobsCards.Add(new Monster("Нулевая реиграбельность", "Когда была всего одна задача и всё равно не справились", 0, Monster.Type.OutsideTroubles));
+        mobsCards.Add(new Monster("Проблемы с балансом", "Даже в тетрисе были квадратные блоки", 0,
+            Monster.Type.OutsideTroubles));
+        mobsCards.Add(new Monster("Нулевая реиграбельность", "Когда была всего одна задача и всё равно не справились",
+            0, Monster.Type.OutsideTroubles));
         mobsCards.Add(new Monster("СЛОЖНААААА", "ААААААААААА", 0, Monster.Type.OutsideTroubles));
     }
 
