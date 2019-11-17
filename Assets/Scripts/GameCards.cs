e97723af50fe4a8202f0d20aea01bcfe19426aa1
kola1197 <kola-1197@mail.ru>
2019-11-17 13:27:18 +0300

    init

---
+++ Assets/Scripts/GameCards.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameCards : MonoBehaviour
+{
+
+    Stack<Card> cards;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void GenerateCards()
+    {
+
+    }
+
+    Card GetCard()
+    {
+        return cards.Pop();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
